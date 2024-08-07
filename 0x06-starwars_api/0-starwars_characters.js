77b248eaa285fcaee8495a0d222cd41bd3310016
shehabbmohamed123 <shehab12p@gmail.com>
2024-08-07 19:10:53 +0300

    a

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3 || isNaN(process.argv[2])) {
+  process.exit(1);
+}
+
+const filmId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const charactersUrls = JSON.parse(body).characters;
+  const characterPromises = charactersUrls.map((characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          reject(charError);
+        } else {
+          resolve(JSON.parse(charBody).name);
+        }
+      });
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then((names) => {
+      names.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((promiseError) => {
+      console.error(promiseError);
+    });
+});
