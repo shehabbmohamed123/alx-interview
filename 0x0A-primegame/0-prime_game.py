cb1bdc0f807781786f1e59957ce912dcfe25df8e
shehabbmohamed123 <shehab12p@gmail.com>
2024-09-04 21:17:23 +0300

    a

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Algoritm to determine the winner of the primegame for given rounds
+and chosen nums"""
+
+
+def isWinner(x, nums):
+    """returns the winner the winner"""
+    if x < 1 or not nums:
+        return None
+    marias_wins, bens_wins = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i * i, n + 1, i):
+            primes[j - 1] = False
+    for _, n in zip(range(x), nums):
+        primes_count = sum(primes[:n])
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
